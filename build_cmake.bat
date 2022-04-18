eb27cfb4704be85bc51cd529b87fec2569fdfcd8
Ger Hobbelt <ger@hobbelt.com>
2022-04-18 21:45:06 +0200

    renamed text files
    delete user-specific batch file which came in with the latest merges.

--- build_cmake.bat
+++ build_cmake.bat
@@ -1,6 +1,7 @@
-if not exist build mkdir build
-cd build
-cmake -G "Visual Studio 16 2019" ..
+if not exist b mkdir b
+cd b
+rem cmake -G "Visual Studio 16 2019" ..
+cmake ..
 cmake --build . --target qjsc --config Release
 
 cd ..
@@ -8,7 +9,7 @@ set qjsc=".\build\Release\qjsc.exe"
 %qjsc% -c -o repl.c -m repl.js
 rem %qjsc% -c -o qjscalc.c qjscalc.js
 
-cd build
+cd b
 cmake ..
 cmake --build . --target qjs --config Release
 
