2b11508366f8dd577918ad3f36312f017f5bab89
zhenweijin <zhenwei.jin@intel.com>
2023-03-26 23:58:47 +0800

    add external array tag

--- quickjs.h
+++ quickjs.h
@@ -86,7 +86,8 @@ enum {
     JS_TAG_FLOAT64     = 7,
     JS_TAG_EXT_OBJ     = 8,
     JS_TAG_EXT_FUNC    = 9,
-    JS_TAG_EXT_INFC    = 10
+    JS_TAG_EXT_INFC    = 10,
+    JS_TAG_EXT_ARRAY   = 11
     /* any larger tag is FLOAT64 if JS_NAN_BOXING */
 };
 
@@ -129,7 +130,7 @@ static inline JS_BOOL JS_VALUE_IS_NAN(JSValue v)
 {
     return 0;
 }
-    
+
 #elif defined(JS_NAN_BOXING)
 
 typedef uint64_t JSValue;
@@ -194,7 +195,7 @@ static inline JS_BOOL JS_VALUE_IS_NAN(JSValue v)
     tag = JS_VALUE_GET_TAG(v);
     return tag == (JS_NAN >> 32);
 }
-    
+
 #else /* !JS_NAN_BOXING */
 
 typedef union JSValueUnion {
@@ -960,7 +961,7 @@ static inline JSValue JS_NewCFunctionMagic(JSContext *ctx, JSCFunctionMagic *fun
 {
     return JS_NewCFunction2(ctx, (JSCFunction *)func, name, length, cproto, magic);
 }
-void JS_SetConstructor(JSContext *ctx, JSValueConst func_obj, 
+void JS_SetConstructor(JSContext *ctx, JSValueConst func_obj,
                        JSValueConst proto);
 
 /* C property definition */
